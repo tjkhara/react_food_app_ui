41eae790ceffff4aa9b8e1015b1d08bb8547f0a2
Tajeshwar <tkhara@gmail.com>
2021-09-08 09:25:02 +0530

    First commit. Video 139 done.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
+function App() {
+  return (
+    <Fragment>
+      <Cart />
+      <Header />
+      <Meals />
+    </Fragment>
+  );
+}
+
+export default App;
